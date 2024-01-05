603267e8e5f735a110a6803bcedeb7112350ad7f
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-01-05 19:01:58 +0200

    refactor Map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,38 +1,76 @@
-import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
-import { Location } from "../../types/interfaces";
+import React, { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { Apartment } from "../../types/interfaces";
+import markerIconURL from "../../images/icons/icons8-location-30.png";
+import L from "leaflet";
+import icon from "../../constants/icon";
 import "leaflet/dist/leaflet.css";
 
+const markerIcon = new L.Icon({
+  iconUrl: markerIconURL,
+  iconSize: [30, 30],
+  iconAnchor: [5, 30],
+});
+
 interface MapProps {
-  locations: Location[];
-  handleMarkerClick: (location: Location) => void;
+  apartments: Apartment[];
+  handleMarkerClick: (apartment: Apartment) => void;
+  handleMapClick: () => void;
 }
 
-const Map: React.FC<MapProps> = ({ locations, handleMarkerClick }) => {
-  const center: LatLngExpression = [51.505, -0.09];
+const Map: React.FC<MapProps> = ({
+  apartments,
+  handleMarkerClick,
+  handleMapClick,
+}) => {
+  const [position, setPosition] = useState<L.LatLng>();
+  // const map = useMapEvents({
+  //   click() {
+  //     map.locate();
+  //   },
+  //   locationfound(e) {
+  //     setPosition(e.latlng);
+  //     map.flyTo(e.latlng, map.getZoom());
+  //   },
+  // });
+  // const map = useMap();
+
+  // useMapEvents({
+  //   click: (e) => {
+  //     const { lat, lng } = e.latlng;
+  //     L.marker([lat, lng], { icon }).addTo(map);
+  //   },
+  // });
 
   return (
     <MapContainer
-      center={center}
-      zoom={13}
+      center={[49.0, 31.0]}
+      zoom={6}
       scrollWheelZoom={false}
-      style={{ height: "500px", width: "800px" }}
+      style={{ height: "600px", width: "800px" }}
+      // onClick={handleMapClick}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright%22%3EOpenStreetMap</a> contributors'
       />
-      {locations.map((location) => (
+      {apartments.map((apartment) => (
         <Marker
-          key={location.id}
-          position={[location.latitude, location.longitude]}
-          // onClick={() => handleMarkerClick(location)}
+          icon={markerIcon}
+          key={apartment.id}
+          position={[apartment.lat, apartment.lon]}
           eventHandlers={{
-            click: () => handleMarkerClick(location),
+            click: () => handleMarkerClick(apartment),
           }}
         >
-          <Popup>{location.name}</Popup>
+          <Popup>{apartment.description}</Popup>
         </Marker>
       ))}
     </MapContainer>
