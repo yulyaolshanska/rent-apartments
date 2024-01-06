fed4344d1beae6a37919fb813af7881888361750
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-01-06 15:07:21 +0200

    feat: add ApartmentList

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,31 @@
 import React, { useState } from "react";
-// import AddAdvertisement from "./components/addAdvertisement/AddAdvertisement";
 import Map from "./components/map/Map";
 import { Apartment } from "./types/interfaces";
 import "leaflet/dist/leaflet.css";
 import { apartmentList } from "./mock/apartments";
 import ApartmentCard from "./components/apartmentCard/ApartmentCard";
 import styles from "./App.module.scss";
-
-const exampleLocations = [
-  {
-    id: 1,
-    name: "Example Location 1",
-    latitude: 51.51,
-    longitude: -0.1,
-  },
-  {
-    id: 2,
-    name: "Example Location 2",
-    latitude: 51.52,
-    longitude: -0.08,
-  },
-];
+import ApartmentList from "./components/apartmentList/ApartmentList";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store/store";
+import AddApartmentForm from "./components/addApartmentForm/AddApartmentForm";
 
 const App: React.FC = () => {
-  const [apartments, setApartments] = useState<Apartment[]>(apartmentList);
   const [selectedApartment, setSelectedApartment] = useState<Apartment | null>(
     null
   );
+  const [apartments, setApartments] = useState<Apartment[]>(apartmentList);
+  const visibleApartments = useSelector(
+    (state: RootState) => state.apartments.apartments
+  );
 
   const handleMarkerClick = (apartment: Apartment) => {
-    console.log("Marker clicked:", apartment);
     setSelectedApartment(apartment);
   };
 
   const handleMapClick = () => {
     setSelectedApartment(null);
   };
-
-  // const handleAdvertisementAdded = (newAdvertisement: Location) => {
-  //   setLocations([...locations, newAdvertisement]);
-  // };
 
   return (
     <div className={styles.pageContainer}>
@@ -48,19 +34,19 @@ const App: React.FC = () => {
         handleMarkerClick={handleMarkerClick}
         handleMapClick={handleMapClick}
       />
-      {/* <AddAdvertisement onAdvertisementAdded={handleAdvertisementAdded} /> */}
-      <div style={{ marginLeft: "10px", width: "300px" }}>
-        {selectedApartment && (
-          <ApartmentCard
-            title={selectedApartment.title}
-            description={selectedApartment.description}
-            type={selectedApartment.type}
-            address={selectedApartment.address}
-            image={selectedApartment.image}
-            price={selectedApartment.price}
-          />
-        )}
-      </div>
+      {selectedApartment ? (
+        <ApartmentCard
+          title={selectedApartment.title}
+          description={selectedApartment.description}
+          type={selectedApartment.type}
+          address={selectedApartment.address}
+          image={selectedApartment.image}
+          price={selectedApartment.price}
+        />
+      ) : (
+        <ApartmentList apartments={visibleApartments} />
+      )}
+      <AddApartmentForm />
     </div>
   );
 };
---
+++ src/components/apartmentList/ApartmentList.tsx
@@ -0,0 +1,29 @@
+import useVisibleApartments from "../../hooks/useVisibleApartments";
+import { Apartment } from "../../types/interfaces";
+import ApartmentCard from "../apartmentCard/ApartmentCard";
+import styles from "./ApartmentList.module.scss";
+
+interface ApartmentsProps {
+  apartments: Apartment[];
+}
+
+const ApartmentList: React.FC<ApartmentsProps> = ({ apartments }) => {
+
+  return (
+    <ul className={styles.apartmentList}>
+      {apartments.map((apartment) => (
+        <ApartmentCard
+          key={apartment.id}
+          title={apartment.title}
+          description={apartment.description}
+          type={apartment.type}
+          address={apartment.address}
+          image={apartment.image}
+          price={apartment.price}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ApartmentList;
