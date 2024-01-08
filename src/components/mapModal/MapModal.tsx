5fb08f43ebfb0b42005447711913eeb9c08bb541
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-01-08 16:59:44 +0200

    feat: handleBackdrop

--- src/components/mapModal/MapModal.tsx
+++ src/components/mapModal/MapModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -26,25 +26,44 @@ const MapModal: React.FC<MapModalProps> = ({
     onSelectLocation(lat, lng);
   };
 
+  const handleBackdrop = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return (): void => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isOpen]);
+
   return (
     <div
-      className={`${styles.mapContainer} ${isOpen ? styles.showContainer : ""}`}
+      className={`${styles.modalBackdrop} ${
+        isOpen ? styles.showContainer : ""
+      }`}
+      onClick={handleBackdrop}
     >
-      <MapContainer
-        center={mapCenter}
-        zoom={6}
-        scrollWheelZoom={false}
-        style={{ height: "400px", width: "800px" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker icon={markerIcon} position={mapCenter}>
-          <Popup>Your selected location</Popup>
-        </Marker>
-        <MapMarker handleMapClick={handleMapClick} />
-      </MapContainer>
+      <div className={styles.mapContainer}>
+        <MapContainer
+          center={mapCenter}
+          zoom={6}
+          scrollWheelZoom={false}
+          style={{ height: "600px", width: "800px" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker icon={markerIcon} position={mapCenter}>
+            <Popup>Your selected location</Popup>
+          </Marker>
+          <MapMarker handleMapClick={handleMapClick} />
+        </MapContainer>
+        <button onClick={onClose}>Зберегти розташування</button>
+      </div>
     </div>
   );
 };
