ff2870f59913891b358ce2876944da8eaf62d5c2
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-01-08 22:01:24 +0200

    feat: header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,20 @@
-const Header = () => {
-  return <div>Header</div>;
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../button/Button";
+import styles from "./Header.module.scss";
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <div className={styles.container}>
+          <Button variant="headerBtn">
+            <Link to={"/add"}>Здати в оренду</Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
